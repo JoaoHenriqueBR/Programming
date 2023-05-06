777ea71efc7db545a39cbf665a576820f7f12b5e
JoaoHenriqueBR <jjb9362@gmail.com>
2023-05-05 21:39:22 -0300

    modified:   Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/053-palindromo.py

--- Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/053-palindromo.py
+++ Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/053-palindromo.py
@@ -1,3 +1,20 @@
 # Crie um programa que leia uma frase qualquer e diga se ela é um palindromo, desconsiderando os espaços.
 # Ex: apos a sopa
 
+frase = str(input('Digite uma frase: ')).strip().lower().replace(' ', '')
+tam = len(frase)
+palindromo = 0
+
+
+for c in range(tam):
+    if ('{}'.format(frase[c])) == ('{}'.format(frase[tam-(c+1)])):
+        palindromo = palindromo + 1
+
+if tam == palindromo:
+    print('É um palindromo! ')
+else:
+    print('NÃO é um palindromo! ')
+
+
+
+
