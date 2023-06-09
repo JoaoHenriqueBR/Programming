f04aa503ec6c1c82b683f8cb3080e5872c423baf
JoaoHenriqueBR <jjb9362@gmail.com>
2023-06-09 17:52:30 -0300

    modified:   "Python/FATEC - Algoritmo e L\303\263gica de Programa\303\247\303\243o/Python - Lista 01 - P2/EX-13.py" 	modified:   Python/rename.py

--- Python/rename.py
+++ Python/rename.py
@@ -1,4 +1,4 @@
-# Rename files
+# Rename files (Currently in "dev" version, maybe I will turn this into a project in the future (Like a File Organizer script))
 
 import os
 
@@ -7,8 +7,9 @@ directory = input('Name of the directory: ')
 os.chdir(directory)
 
 c = 0
-for f in os.listdir():
-    c += 1
-    file_name, file_ext = os.path.splitext(f)
+for f in os.listdir(): # For each file in the directory:
+    c += 1 # Counter
+    file_name, file_ext = os.path.splitext(f) # split between file name and file extension
     new_name = '{} - EX-{}{}'.format(file_name, c, file_ext) # Choose the new name
-    os.rename(f, new_name)
+#    print(new_name) # Show the new name
+    os.rename(f, new_name) # Change the files to the new name
