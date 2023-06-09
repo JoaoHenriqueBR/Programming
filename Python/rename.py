36887d758a7ce4779237122751ac1975c95ea94a
JoaoHenriqueBR <jjb9362@gmail.com>
2023-06-09 17:54:01 -0300

    modified:   Python/rename.py

--- Python/rename.py
+++ Python/rename.py
@@ -11,5 +11,5 @@ for f in os.listdir(): # For each file in the directory:
     c += 1 # Counter
     file_name, file_ext = os.path.splitext(f) # split between file name and file extension
     new_name = '{} - EX-{}{}'.format(file_name, c, file_ext) # Choose the new name
-#    print(new_name) # Show the new name
+    print(f'Renamed to: {new_name}') # Show the new name
     os.rename(f, new_name) # Change the files to the new name
