628b798d167802b6e13824bc14d4e1bd1920ab70
JoaoHenriqueBR <jjb9362@gmail.com>
2023-06-25 10:38:44 -0300

    modified:   Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/054-maioridade.py

--- Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/054-maioridade.py
+++ Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/054-maioridade.py
@@ -2,3 +2,18 @@
 # ainda não atingiram a maioridade e quantas já são maiores.
 # Maioridade = 21 anos
 
+from datetime import date
+
+maior = menor = 0
+
+
+for p in range(10):
+    n = int(input('Digite seu ano de nascimento: '))
+    idade = date.today().year - n
+    if idade >= 21:
+        maior += 1
+    else:
+        menor += 1
+
+print(f'{maior} pessoas atingiram a maioridade.')
+print(f'{menor} pessoas não a atingiram.')
