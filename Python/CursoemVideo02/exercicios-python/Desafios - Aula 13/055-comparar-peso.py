1ce2800065ef61eb496f4d080cb8ac71d3f9de6c
JoaoHenriqueBR <jjb9362@gmail.com>
2023-06-25 10:43:02 -0300

    modified:   Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/055-comparar-peso.py

--- Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/055-comparar-peso.py
+++ Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/055-comparar-peso.py
@@ -1,1 +1,15 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor.
+
+from math import inf
+
+maior = 0
+menor = inf
+for p in range(5):
+    peso = float(input('Digite seu peso (KG): '))
+    if peso > maior:
+        maior = peso
+    if peso < menor:
+        menor = peso
+
+print(f'O maior peso foi: {maior} KG')
+print(f'O menor foi: {menor} KG')
