835f31ca7baf22599def3ad2982d4ec8aa366b3c
JoaoHenriqueBR <jjb9362@gmail.com>
2023-07-04 12:47:46 -0300

    modified:   Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/046-contagem-regressiva.py

--- Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/046-contagem-regressiva.py
+++ Python/CursoemVideo02/exercicios-python/Desafios - Aula 13/046-contagem-regressiva.py
@@ -7,7 +7,7 @@ print("~="*20)
 print('FOGOS DE ARTIFICIO - CONTAGEM REGRESSIVA')
 print("~="*20)
 
-for c in range(10, 0, -1):
+for c in range(10, -1, -1):
     print(c)
     sleep(1)
 print('\033[31m KABUUMM!!! \033[m')
