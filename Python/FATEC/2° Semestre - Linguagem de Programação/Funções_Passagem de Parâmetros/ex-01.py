49657e07a28929d659f93ec7638928c52cea4539
JoaoHenriqueBR <jjb9362@gmail.com>
2023-09-30 16:34:13 -0300

    Exercicios 03/10

---
+++ Python/FATEC/2° Semestre - Linguagem de Programação/Funções_Passagem de Parâmetros/ex-01.py
@@ -0,0 +1,7 @@
+# Crie uma função chamada somaque recebe dois inteiros como parâmetros e retorna a soma deles
+
+def somaque(a, b):
+    soma = a + b
+    return soma
+
+print('Soma: {}'.format(somaque(a=int(input('Digite um número: ')), b=int(input('Digite outro: ')))))
