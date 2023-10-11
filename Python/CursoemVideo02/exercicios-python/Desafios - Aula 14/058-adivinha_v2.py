f34798d4d38c3496523e92ad6da29130420f15f6
JoaoHenriqueBR <jjb9362@gmail.com>
2023-10-11 20:55:47 -0300

    Aula 14

---
+++ Python/CursoemVideo02/exercicios-python/Desafios - Aula 14/058-adivinha_v2.py
@@ -0,0 +1,2 @@
+# Melhore o jogo do Desafio 28 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar advinhar até acertar.
+# Mostrando no final quantos palpites foram necessários para vencer.
---
+++ Python/CursoemVideo02/exercicios-python/Desafios - Aula 14/059-menu.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia dois valores e mostre um menu na tela:
+#
+# [1] Soma;
+# [2] Multiplicação;
+# [3] Maior;
+# [4] Novos números;
+# [5] Sair do programa.
+#
+# Seu programa deverá realizar a operação solicitada em cada caso.
+#
+
