1d04ea299ecfe474256f4b78d59dcd4961bf6d78
JoaoHenriqueBR <jjb9362@gmail.com>
2024-02-27 23:48:00 -0300

    Exercicio 7 - Páginas 25 e 26

--- C e C++/FATEC/3º Semestre - Estrutura de Dados/Exercicios/Trab1/L01D.cpp
+++ C e C++/FATEC/3º Semestre - Estrutura de Dados/Exercicios/Trab1/L01D.cpp
@@ -2,10 +2,16 @@
 #include "math.h"
 #include "cstdlib"
 #include <stdio.h> // getchar (Linux)
+#include <iomanip> // setprecision (Limitar casas decimais)
 using namespace std;
 
 /*
-  Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz 12 Km por litro. Para obter o cálculo, o usuário deve fornecer o tempo gasto (TEMPO) e a velocidade média (VELOCIDADE) durante a viagem. Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA ← TEMPO * VELOCIDADE. Possuindo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem com a fórmula LITROS_USADOS ← DISTANCIA / 12. Ao final, o programa deve apresentar os valores da velocidade média (VELOCIDADE) e a quantidade de litros (LITROS_USADOS) utilizada na viagem.
+  Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz 12 Km por litro. 
+  Para obter o cálculo, o usuário deve fornecer o tempo gasto (TEMPO) e a velocidade média (VELOCIDADE) durante a viagem. 
+  Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA ← TEMPO * VELOCIDADE. 
+  Possuindo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem com a fórmula LITROS_USADOS ← DISTANCIA / 12. 
+  Ao final, o programa deve apresentar os valores da velocidade média (VELOCIDADE), tempo gasto na viagem (TEMPO), a distância percorrida (DISTANCIA)
+  e a quantidade de litros (LITROS_USADOS) utilizada na viagem.
 */
 
 int main()
@@ -24,7 +30,12 @@ int main()
   litros_usados = distancia / 12;
 
   cout << "\nA velocidade média foi de " << velocidade << " km/h.";
-  cout << "\nA quantidade de litros utilizada na viagem foi de " << litros_usados << endl;
+  cout << "\nForam gastos " << tempo << " horas na viagem.";
+  cout << "\nFoi percorrido uma distância de " << distancia << " km.";
+
+  cout << fixed << setprecision(1); // Limita a casa decimal do próximo cout, assim o arredondando.
+
+  cout << "\nA quantidade de litros utilizada na viagem foi de " << litros_usados << " litros" << endl;
 
   int c = getchar(); // No Windows: system("pause");
   
