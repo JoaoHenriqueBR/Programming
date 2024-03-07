e0f3bcbcfcbf4f435111afc9c3a44e896af09fa9
JoaoHenriqueBR <jjb9362@gmail.com>
2024-03-07 17:13:35 -0300

    EX 066

--- Python/CursoemVideo02/exercicios-python/Desafios - Aula 15/066.py
+++ Python/CursoemVideo02/exercicios-python/Desafios - Aula 15/066.py
@@ -2,3 +2,14 @@
 Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
 No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
 '''
+
+c = soma = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    c += 1
+    soma += n
+
+print(f'Qtd. de Números digitados: {c}')
+print(f'Soma: {soma}')
