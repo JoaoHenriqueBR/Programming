38c88546e2c1c706857e5feaec0f3f8fe6d41c27
JoaoHenriqueBR <jjb9362@gmail.com>
2024-03-07 17:14:44 -0300

    EX 069

--- Python/CursoemVideo02/exercicios-python/Desafios - Aula 15/069.py
+++ Python/CursoemVideo02/exercicios-python/Desafios - Aula 15/069.py
@@ -7,3 +7,24 @@ A) Quantas pessoas tem mais de 18 anos.
 B) Quantos homens foram cadastrados.
 C) Quantas mulheres com menos de 20 anos.
 '''
+
+maior = garota = homem = 0
+while True:
+    print('-'*30)
+    print(f'{'CADASTRE UMA PESSOA':^30}')
+    print('-'*30)
+    idade = int(input('IDADE: '))
+    sexo = input('Sexo (M/F): ').strip().upper()
+    if sexo == 'M':
+        homem += 1
+    if idade >= 18:
+        maior += 1
+    if idade < 20 and sexo == 'F':
+        garota += 1
+    continuar = input('Deseja continuar? [S/N]: ').strip().upper()
+    if continuar == 'N':
+        break
+
+print(f'{maior} pessoas acima de 18.')
+print(f'{homem} homens.')
+print(f'{garota} mulheres com menos de 20.')
