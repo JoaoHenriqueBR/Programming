a936d791f1f008f4d5cacddb2716c2c6ea3f2eba
JoaoHenriqueBR <jjb9362@gmail.com>
2024-03-07 17:13:56 -0300

    EX 067

--- Python/CursoemVideo02/exercicios-python/Desafios - Aula 15/067.py
+++ Python/CursoemVideo02/exercicios-python/Desafios - Aula 15/067.py
@@ -1,2 +1,12 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário.
 # O programa será interrompido quando o número solicitado for negativo.
+
+while True:
+    n = int(input('Número que deseja saber a tabuada (Negativo = Parar): '))
+    if n < 0:
+        break
+    for c in range(1, 11):
+        print(f'{n} x {c} = {n*c}')
+
+print('')
+print('Programa finalizado com sucesso... ')
