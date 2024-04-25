d12d6c2b28ad4e9ec416c86f40c060657d51bfcc
JoaoHenriqueBR <jjb9362@gmail.com>
2024-04-25 14:39:09 -0300

    EX 070

--- Python/CursoemVideo02/exercicios-python/Desafios - Aula 15/070.py
+++ Python/CursoemVideo02/exercicios-python/Desafios - Aula 15/070.py
@@ -8,3 +8,36 @@ B) Quantos produtos custam mais de R$ 1000.
 C) Qual é o nome do produto mais barato.
 '''
 
+from time import sleep
+
+c = preco = caros = soma = 0
+r = 's'
+while r == 's':
+    c += 1
+    print('-'*30)
+    nome = f'Produto {c}'
+    print(f' {nome:^30}')
+    print('-'*30)
+    nome = input('Nome do produto: ')
+    preco = float(input('Preço de produto: R$ '))
+    print('Produto registrado...')
+    soma += preco
+    if preco > 1000:
+        caros += 1
+    if c == 1:
+        nome_barato = nome
+        barato = preco
+    elif preco < barato:
+        nome_barato = nome
+        barato = preco
+    sleep(1)
+    while r != 'n':
+        r = input('Deseja registrar outro? (s/n): ').lower()
+        if r == 's':
+            break
+
+print('')
+print(f'TOTAL GASTO: R$ {soma:.2f}')
+print(f'Produtos acima de R$ 1000: {caros}')
+print(f'Produto mais barato: {nome_barato}, R$ {barato:.2f}')
+
