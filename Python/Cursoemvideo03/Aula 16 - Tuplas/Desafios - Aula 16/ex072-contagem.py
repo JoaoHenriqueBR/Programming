b916ad6ec3aad9e8298a40b97440e585333c281d
JoaoHenriqueBR <jjb9362@gmail.com>
2024-07-23 01:42:50 -0300

    EX 072 (Finalizado)

--- Python/Cursoemvideo03/Aula 16 - Tuplas/Desafios - Aula 16/ex072-contagem.py
+++ Python/Cursoemvideo03/Aula 16 - Tuplas/Desafios - Aula 16/ex072-contagem.py
@@ -6,3 +6,20 @@ de zero até vinte,
 Seu programa deverá ler um número pelo teclado (entre 0 e 20)
 e mostrá-lo por extenso.
 '''
+
+extenso = ('zero', 'um', 'dois', 'três', 'quatro', 
+            'cinco', 'seis', 'sete', 'oito', 'nove',
+            'dez', 'onze', 'doze', 'treze', 'quatorze', 
+            'quinze', 'dezesseis', 'dezessete', 'dezoito', 
+            'dezenove', 'vinte')
+
+while True:
+    n = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= n <= 20:
+        print(extenso[n])
+        r = input('deseja continuar? (s/n): ').lower().strip()
+        if r == 'n':
+            print('\nPrograma Finalizado! Obrigado por usar-lo.')
+            break
+    else:
+        print('Valor errado! Tente novamente.')
