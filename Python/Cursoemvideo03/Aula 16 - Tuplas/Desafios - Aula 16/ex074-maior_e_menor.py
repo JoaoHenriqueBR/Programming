cd0829cc08af1893df0e3b9ab7efd5c5083feb6f
JoaoHenriqueBR <jjb9362@gmail.com>
2024-07-23 01:43:37 -0300

    EX 074 (Finalizado)

--- Python/Cursoemvideo03/Aula 16 - Tuplas/Desafios - Aula 16/ex074-maior_e_menor.py
+++ Python/Cursoemvideo03/Aula 16 - Tuplas/Desafios - Aula 16/ex074-maior_e_menor.py
@@ -5,3 +5,16 @@ e colocar em uma tupla.
 Depois disso, mostre a listagem de números gerados e
 também indique o menor e o maior valor que estão na tupla.
 '''
+
+from random import randint
+
+
+tupla = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+
+print('Foram sorteados: ', end='')
+for n in tupla:
+    print(f'{n} ', end='')
+
+print('')
+print(f'Maior: {max(tupla)}')
+print(f'Menor: {min(tupla)}')
