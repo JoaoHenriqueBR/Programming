52d9f5e996960cd4b0a6b0c6aa62d2e7a626222f
JoaoHenriqueBR <jjb9362@gmail.com>
2024-07-23 01:43:04 -0300

    EX 073 (Finalizado)

--- Python/Cursoemvideo03/Aula 16 - Tuplas/Desafios - Aula 16/ex073-tabela_de_futebol.py
+++ Python/Cursoemvideo03/Aula 16 - Tuplas/Desafios - Aula 16/ex073-tabela_de_futebol.py
@@ -6,6 +6,27 @@ na ordem de colocação. Depois mostre:
 A) Apenas os 5 primeiros colocados.
 B) Os últimos 4 colocados da tabela.
 C) Uma lista com os times em Ordem Alfabética.
-D) Em que posição da tabela está o time do Cuiabá.
+D) Em que posição da tabela está o time do Cuiabá. <-- O Time original do video, Chapecoense está na série B em 2024
 '''
+
+tabela = ('Botafogo', 'Palmeiras', 'Flamengo', 'Fortaleza', 'São Paulo', 'Bahia', 'Cruzeiro', 'Athletico-PR', 'Bragantino', 'Atlético-MG', 'Vasco', 'Juventude', 'Internacional', 'Corinthians', 'Criciúma', 'Cuiabá', 'Vitória', 'Grêmio', 'Fluminense', 'Atlético-GO')
+
+print('='*20)
+print(f'{"BRASILEIRÃO 2024":^20}')
+print('='*20)
+for pos, time in enumerate(tabela):
+    print(f'{pos+1}º - {time}')
+print('')
+print('-> 22 de julho de 2024')
+
+
+print('-'*20)
+print(f'5 Primeiros colocados: {tabela[:5]}')
+print('')
+print(f'Últimos 4 colocados: {tabela[-4:]}')
+print('')
+print(f'Ordem Alfabética: {sorted(tabela)}')
+print('')
+print(f'Cuiabá está na {tabela.index("Cuiabá")+1}ª posição')
+print('-'*20)
 
