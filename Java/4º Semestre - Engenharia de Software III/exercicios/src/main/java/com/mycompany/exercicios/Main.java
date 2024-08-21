570e8f1161b77ffea08609c259b6c9045f24d755
JoaoHenriqueBR <jjb9362@gmail.com>
2024-08-21 02:38:00 -0300

    ADD: Exercicios - 14/08 > 21/08

---
+++ Java/4º Semestre - Engenharia de Software III/exercicios/src/main/java/com/mycompany/exercicios/Main.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exercicios;
+import java.util.Scanner;
+
+/**
+ *
+ * @author joao
+ */
+public class Main {
+    public static int soma(int a, int b){
+        return a + b;
+    }
+    
+    public static int subtracao(int a, int b){
+        return a - b;
+    }
+    
+    public static int multiplicacao(int a, int b){
+        return a * b;
+    }
+    
+    public static double divisao(int a, int b){
+        return (double) a / (double) b;
+    }
+    
+    public static void main(String[] args){
+        int a, b;
+        
+            
+        Scanner input = new Scanner(System.in);
+        
+        Robo R2D2 = new Robo();
+        R2D2.setNome("R2D2");
+        R2D2.setData("20/08/2024");
+        R2D2.setBateria(10);
+        
+        System.out.println("Olá, sou um robô e me chamo " + R2D2.getNome());
+        System.out.println("Fui fabricado em " + R2D2.getData());
+        
+        for (int i = 0; i < R2D2.getBateria(); i++){
+            System.out.println("Tenho " + (R2D2.getBateria() - i) + "% de bateria restando");
+            
+            System.out.print("Digite um número (0 > Parar): ");
+            a = input.nextInt();
+            
+            if (a == 0) break;
+
+            
+            System.out.print("Digite outro (0 > Parar): ");
+            b = input.nextInt();
+            
+            if (b == 0) break;
+            
+            System.out.println("\nSoma: " + soma(a, b));
+            System.out.println("Subtração: " + subtracao(a, b));
+            System.out.println("Multiplicação: " + multiplicacao(a, b));
+            System.out.println("Divisão: " + divisao(a, b));
+            
+            
+        }
+        
+        System.out.println("\nDesligando R2D2...");
+    }
+}
---
+++ Java/4º Semestre - Engenharia de Software III/exercicios/src/main/java/com/mycompany/exercicios/Robo.java
@@ -0,0 +1,37 @@
+package com.mycompany.exercicios;
+
+
+public class Robo {
+
+	private String nome;
+
+	private String data;
+
+	private int bateria;
+
+	public String getNome() {
+            return nome;
+	}
+
+	public void setNome(String nome) {
+            this.nome = nome;
+	}
+
+	public String getData() {
+            return data;
+	}
+
+	public void setData(String data) {
+            this.data = data;
+	}
+
+	public int getBateria() {
+            return bateria;
+	}
+
+	public void setBateria(int bateria) {
+            this.bateria = bateria;
+	}
+        
+
+}
