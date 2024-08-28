5aa2f704ef34a72914dcdc381dd9268dfc8a2d4c
JoaoHenriqueBR <jjb9362@gmail.com>
2024-08-27 22:30:27 -0300

    EX 075 (Finalizado)

--- Python/Cursoemvideo03/Aula 16 - Tuplas/Desafios - Aula 16/ex075-analise_de_dados.py
+++ Python/Cursoemvideo03/Aula 16 - Tuplas/Desafios - Aula 16/ex075-analise_de_dados.py
@@ -7,3 +7,21 @@ A) Quantas vezes apareceu o valor 9.
 B) Em que posição foi digitado o primeiro valor 3.
 C) Quais foram os números pares.
 '''
+
+tupla = (int(input("Digite um número: ")), int(input("Digite o segundo número: ")), int(input("Digite o terceiro número: ")), int(input("Digite o ṹltimo: ")))
+
+print("")
+print(f"Você digitou: {tupla}")
+print(f"O 9 apareceu {tupla.count(9)} vezes.")
+
+if 3 in tupla:
+    print(f"O 3 apareceu na {tupla.index(3)+1}º posição.")
+else:
+    print("O valor 3 não foi digitado.")
+
+print("São valores pares: ", end="")
+for n in tupla:
+    if n % 2 == 0:
+       print(n, end=' ')
+
+print("")
