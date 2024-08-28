a4ced5e7101d09f8ef9d01452b0cd325953ab9d1
JoaoHenriqueBR <jjb9362@gmail.com>
2024-08-27 22:30:47 -0300

    EX 076 (Finalizado)

--- Python/Cursoemvideo03/Aula 16 - Tuplas/Desafios - Aula 16/ex076-precos.py
+++ Python/Cursoemvideo03/Aula 16 - Tuplas/Desafios - Aula 16/ex076-precos.py
@@ -6,3 +6,14 @@ na sequência.
 No final, mostre uma listagem de preços, 
 organizando os dados em forma tabular
 '''
+
+tabela = ("Pão", 1.8, "Hamburguer", 5, "Coxinha", 4, "Pão de Queijo", 3)
+
+print('-'*43)
+print(f'{"TABELA DE PREÇOS DO JH":^43}')
+print('-'*43)
+
+for i in range(0, len(tabela), 2):
+    print(f"{tabela[i]:.<30} | R$ {tabela[1+i]:>7.2f}")
+
+print('-'*43)
