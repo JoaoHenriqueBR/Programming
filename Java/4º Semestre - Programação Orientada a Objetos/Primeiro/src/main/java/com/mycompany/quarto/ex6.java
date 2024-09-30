13fb34f3e5ba594c364ef1d6671a1835245e864c
JoaoHenriqueBR <jjb9362@gmail.com>
2024-09-30 13:21:17 -0300

    TODO: Exercicios POO - A4 (6/8)

---
+++ Java/4º Semestre - Programação Orientada a Objetos/Primeiro/src/main/java/com/mycompany/quarto/ex6.java
@@ -0,0 +1,12 @@
+package com.mycompany.quarto;
+import javax.swing.JOptionPane;
+
+/*
+EX6:
+    Criar um programa que receba um número que corresponda a um mês do 1º tremestre e escreva o mês correspondente;
+        caso o usuário digite o número fora do intervalo deverá aparecer inválido, mas utilizando um método do tipo void.
+ */
+
+public class ex6 {
+
+}
