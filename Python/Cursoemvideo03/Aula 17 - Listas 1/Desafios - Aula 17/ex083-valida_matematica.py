a78fa0bb7b57e0cd5d37527e1d2456c1b27732bc
JoaoHenriqueBR <jjb9362@gmail.com>
2024-11-05 18:22:55 -0300

    TODO: Exercicios - Aula 17

---
+++ Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex083-valida_matematica.py
@@ -0,0 +1,4 @@
+'''
+Crie um programa onde o usuário digite uma expressão qualquer que usa parênteses.
+Seu aplicativo deverá analisar se a expressão passada está com os parenteses abertos ou fechados na ordem correta.
+'''
