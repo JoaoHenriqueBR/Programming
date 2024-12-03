76d79f09fa993b2499b2548cd23ef94e4b2f76be
JoaoHenriqueBR <jjb9362@gmail.com>
2024-12-03 05:26:13 -0300

    EX 079 (Finalizado)

--- Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex079-valores_unicos.py
+++ Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex079-valores_unicos.py
@@ -4,3 +4,17 @@
 #   ele não será adicionado.
 # No final, serão exibidos todos os valores únicos digitados,
 #   em ordem crescente.
+
+valores = []
+while True:
+    n = int(input("Digite um número: "))
+    if n in valores:
+        print("Valor já na lista.")
+    else:
+        valores.append(n)
+        print("Valor adicionado com sucesso!")
+    r = input("Deseja continuar? (s/n): ")
+    if r in 'Nn':
+        break
+
+print(f"Foi digitado os valores {sorted(valores)}.")
