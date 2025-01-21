420912dddde63bd08e6f742032dd7aecec138548
JoaoHenriqueBR <jjb9362@gmail.com>
2025-01-21 03:01:20 -0300

    EX 080 (Finalizado)

--- Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex080-lista_ordenada.py
+++ Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex080-lista_ordenada.py
@@ -4,3 +4,20 @@
 #   (sem usar o sort()).
 #
 # No final, mostre a lista ordenada na tela.
+
+numeros = []
+for c in range(0, 5):
+    n = int(input("Digite um número: "))
+    if c == 0 or n > numeros[-1]:
+        numeros.append(n)
+        print('Adicionado ao final da lista...')
+    else:
+        for i, num in enumerate(numeros):
+            if n <= num:
+                numeros.insert(i, n)
+                print(f'Adicionado na posição {i} da lista...')
+                break
+            
+        
+print("-="*30)
+print(f"Lista ordenada: {numeros}")
