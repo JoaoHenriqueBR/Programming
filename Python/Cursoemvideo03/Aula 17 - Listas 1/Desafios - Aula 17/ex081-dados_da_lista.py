ca6e90966ce508f38771931a1ae1fab6019dd687
JoaoHenriqueBR <jjb9362@gmail.com>
2025-02-12 07:14:26 -0300

    EX 081 (Finalizado)

--- Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex081-dados_da_lista.py
+++ Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex081-dados_da_lista.py
@@ -7,3 +7,23 @@ A) Quantos números foram digitados.
 B) A lista de valores, ordenada de forma decrescente.
 C) Se o valor 5 foi digitado e está ou não na lista.
 '''
+
+lista = []
+
+while True:
+    n = int(input("Digite um número: "))
+    lista.append(n)
+    r = input("Deseja continuar? (s/n): ").lower().strip()
+    if r in "n":
+        break
+
+print(f"Lista: {lista}\n")
+lista.sort(reverse=True)
+print(f"Foram digitados {len(lista)} números.")
+print(f"Ordem decrescente: {lista}\n")
+
+if 5 in lista:
+    print(f"O número 5 foi digitado {lista.count(5)} vez(es).")
+else:
+    print("O valor 5 não foi encontrado na lista.")
+    
