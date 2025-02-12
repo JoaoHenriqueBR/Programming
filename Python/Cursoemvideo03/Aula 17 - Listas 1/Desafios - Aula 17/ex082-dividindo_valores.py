47ac738047c17ea9d881dcdc1a4e893b138d3483
JoaoHenriqueBR <jjb9362@gmail.com>
2025-02-12 07:14:42 -0300

    EX 082 (Finalizado)

--- Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex082-dividindo_valores.py
+++ Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex082-dividindo_valores.py
@@ -8,3 +8,23 @@ digitados, respectivamente.
 
 Ao final, mostre o conteúdo das três listas geradas.
 '''
+
+num = []
+pares = num[:]
+impares = num[:]
+
+while True:
+    n = int(input("Digite um número: "))
+    num.append(n)
+    if n % 2 == 0:
+        pares.append(n)
+    else:
+        impares.append(n)
+    
+    r = input("Deseja continuar (s/n): ").strip().lower()
+    if r != "s":
+        break
+
+print(f"\nDigitado: {num}")
+print(f"Pares: {pares}")
+print(f"Impares: {impares}")
