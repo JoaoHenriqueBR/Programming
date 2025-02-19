be69b0e535963c688ebee83af2a1c564a7ffbf0d
JoaoHenriqueBR <jjb9362@gmail.com>
2025-02-18 23:24:53 -0300

    EX 083 (Finalizado)

--- Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex083-valida_matematica.py
+++ Python/Cursoemvideo03/Aula 17 - Listas 1/Desafios - Aula 17/ex083-valida_matematica.py
@@ -2,3 +2,22 @@
 Crie um programa onde o usuário digite uma expressão qualquer que usa parênteses.
 Seu aplicativo deverá analisar se a expressão passada está com os parenteses abertos ou fechados na ordem correta.
 '''
+
+expressao = input("Digite uma expressão matemática: ")
+pilha = []
+
+for simb in expressao:
+    if simb == "(":
+        pilha.append("(")
+    elif simb == ")":
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(")")
+            break
+
+if len(pilha) == 0:
+    print("Sua expressão está válida!")
+else:
+    print("Sua expressão está errada!")
+
