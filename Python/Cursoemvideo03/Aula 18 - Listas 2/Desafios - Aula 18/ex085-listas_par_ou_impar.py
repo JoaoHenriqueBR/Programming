077c489f1d8da5706e6c6afc92ab3ee016658425
João Henrique <70080335+JoaoHenriqueBR@users.noreply.github.com>
2025-04-26 15:55:07 +0000

    EX 085 (Finalizado)

--- Python/Cursoemvideo03/Aula 18 - Listas 2/Desafios - Aula 18/ex085-listas_par_ou_impar.py
+++ Python/Cursoemvideo03/Aula 18 - Listas 2/Desafios - Aula 18/ex085-listas_par_ou_impar.py
@@ -2,3 +2,17 @@
 Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 '''
 
+num = [[], []]
+
+for c in range(7):
+  n = int(input(f'Digite o {c+1}º número: '))
+  if n % 2 == 0:
+    num[0].append(n)
+  else:
+    num[1].append(n)
+
+print('-'*30)
+num[0].sort()
+num[1].sort()
+print(f'Pares: {num[0]}')
+print(f'Impares: {num[1]}')
