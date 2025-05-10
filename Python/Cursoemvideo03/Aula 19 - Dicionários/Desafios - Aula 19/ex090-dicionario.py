7b9145e37b234089362765dbf8594c2d82674069
JoaoHenriqueBR <jjb9362@gmail.com>
2025-05-10 16:47:59 -0300

    EX 090 (Finalizado)

--- Python/Cursoemvideo03/Aula 19 - Dicionários/Desafios - Aula 19/ex090-dicionario.py
+++ Python/Cursoemvideo03/Aula 19 - Dicionários/Desafios - Aula 19/ex090-dicionario.py
@@ -1,4 +1,21 @@
 '''
-Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
+Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+No final, mostre o conteúdo da estrutura na tela.
 '''
 
+aluno = dict()
+
+aluno['nome'] = str(input('Nome do aluno: '))
+aluno['media'] = float(input('Média: '))
+
+if aluno['media'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif aluno['media'] < 5:
+    aluno['situação'] = 'Reprovado'
+else:
+    aluno['situação'] = 'Recuperação'
+
+
+print('-='*15)
+for k, v in aluno.items():
+    print(f'{k}: {v}')
