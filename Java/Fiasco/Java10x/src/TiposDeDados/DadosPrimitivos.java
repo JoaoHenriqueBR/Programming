7d06021ed5f66ac094a79a89b24ade6b73e90f6b
JoaoHenriqueBR <jjb9362@gmail.com>
2025-05-23 19:35:11 -0300

    Java10x: Dados primitivos e o erro de 50 milhoes

---
+++ Java/Fiasco/Java10x/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,22 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+        * Dados primitivos - int, double, float, char, boolean, short.
+        * Objetivos da aula: Criar um ninja - Naruto -
+        * */
+
+        int idade = 16; // Valor máximo: 2 147 483 647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivo = true;
+        Long saldoBancario = 201472839281L; // Valor máximo: 9,223,372,036,854,775,807
+
+        System.out.println(idade); // Comando para mostrar para o usuário
+        System.out.println(saldoBancario);
+        System.out.println(altura);
+        System.out.println("saldoBancario é = " + saldoBancario);
+        System.out.println("Minha idade é = " + idade);
+    }
+}
