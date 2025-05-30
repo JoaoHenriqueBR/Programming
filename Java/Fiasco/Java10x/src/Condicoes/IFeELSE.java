7f840b6a35d8c7ed4b6081077e5e82b5b2cac9cf
JoaoHenriqueBR <jjb9362@gmail.com>
2025-05-30 19:28:46 -0300

    Java10x: Condicionais criando lógica para o Narutinho

---
+++ Java/Fiasco/Java10x/src/Condicoes/IFeELSE.java
@@ -0,0 +1,30 @@
+package Condicoes;
+
+public class IFeELSE {
+    public static void main(String[] args) {
+
+        /*
+        * IF e ELSE - Condições
+        * else if
+        * Objetivo: Passar o ninja de nível de acordo com o número de missões.
+        * */
+
+        // Ninja Naruto
+        String nome = "Naruto Uzumaki";
+        int idade = 10;
+        boolean hokage = false;
+        short numeroDeMissoes = 14;
+
+        String rank; // Inicia uma variável sem valor
+
+        // se (condição) {faça isso}
+        if (numeroDeMissoes < 10){
+            rank = "Gennim";
+        } else if (numeroDeMissoes >= 20) {
+            rank = "Jounin";
+        } else {
+            rank = "Chunnin";
+        }
+        System.out.println("Rank: " + rank);
+    }
+}
