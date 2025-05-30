30c6f6874f2dc59a9457c9bafef3ce4c65159471
JoaoHenriqueBR <jjb9362@gmail.com>
2025-05-30 19:46:34 -0300

    EX 096 (Finalizado)

--- Python/Cursoemvideo03/Aula 20 - Funções 1/Desafios - Aula 20/ex096-area.py
+++ Python/Cursoemvideo03/Aula 20 - Funções 1/Desafios - Aula 20/ex096-area.py
@@ -4,3 +4,16 @@ Faça um programa que tenha uma função chamada área(),
     mostre a área do terreno.
 '''
 
+def area(larg, comp):
+    print(f"Área do terreno {larg}x{comp}: {larg*comp}m².")
+
+
+# Programa Principal
+print('-'*20)
+print(f'{"Controle de Terrenos":^20}')
+print('-'*20)
+
+l = float(input('LARGURA (m): '))
+c = float(input('COMPRIMENTO (m): '))
+
+area(l, c)
