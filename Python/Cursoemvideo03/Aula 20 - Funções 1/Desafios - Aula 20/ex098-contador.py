1f5318eb2196a92a970cbdeb6b04af8886155c2c
JoaoHenriqueBR <jjb9362@gmail.com>
2025-06-02 20:31:09 -0300

    EX 098 (Finalizado)

--- Python/Cursoemvideo03/Aula 20 - Funções 1/Desafios - Aula 20/ex098-contador.py
+++ Python/Cursoemvideo03/Aula 20 - Funções 1/Desafios - Aula 20/ex098-contador.py
@@ -9,3 +9,36 @@ Seu programa tem que realizar três contagens através da função criada:
     c) Uma contagem personaliza.
 '''
 
+from time import sleep
+
+def contagem(inicio, fim, passo):
+    if passo < 0:
+        passo *= -1
+    elif passo == 0:
+        passo = 1
+    
+    print('-='*20)
+    print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
+    sleep(2)
+
+    if inicio > fim:
+        passo *= -1
+        fim -= 1
+    else:
+        fim += 1
+    
+    for c in range(inicio, fim, passo):
+        print(c, end=' ', flush=True)
+        sleep(0.5)
+    print('FIM!')
+    
+
+# Programa Principal
+contagem(1, 10, 1)
+contagem(10,0,2)
+print('-='*20)
+print('Crie sua contagem!')
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+contagem(i, f, p)
