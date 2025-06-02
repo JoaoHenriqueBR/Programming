611ad69c6e1fa004fe9c0918c3f0cef1cdf60ad2
JoaoHenriqueBR <jjb9362@gmail.com>
2025-06-02 20:52:30 -0300

    EX 100 (Finalizado)

--- Python/Cursoemvideo03/Aula 20 - Funções 1/Desafios - Aula 20/ex100-soma_sorteada.py
+++ Python/Cursoemvideo03/Aula 20 - Funções 1/Desafios - Aula 20/ex100-soma_sorteada.py
@@ -4,3 +4,31 @@ Faça um programa que tenha uma lista chamada números e duas funções chamada 
     - A segunda função vai mostrar a soma entre todos os valores PARES sorteados pela função anterior.
 '''
 
+from random import randint
+from time import sleep
+
+números = list()
+
+def sorteia(num):
+    
+    print('Sorteando números: ', end='')
+
+    for c in range(0,5):
+        n = randint(0,10)
+        num.append(n)
+        print(n, end=' ', flush=True)
+        sleep(0.5)
+    print()
+
+
+def somaPar(num):
+    soma = 0
+    
+    for n in num:
+        if n % 2 == 0:
+           soma += n
+
+    print(f'Soma dos pares de {num}: {soma}')
+
+sorteia(números)
+somaPar(números)
