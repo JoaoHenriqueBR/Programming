fb269bef9d8f3eaa797ab3e0b59a61751d022539
JoaoHenriqueBR <jjb9362@gmail.com>
2025-07-28 20:36:53 -0300

    EX 102 (Finalizado)

--- Python/Cursoemvideo03/Aula 21 - Funções 2/Desafios - Aula 21/ex102-fatorial.py
+++ Python/Cursoemvideo03/Aula 21 - Funções 2/Desafios - Aula 21/ex102-fatorial.py
@@ -6,3 +6,39 @@ Crie um programa que tenha uma função fatorial() que receba dois parâmetros:
     - Indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
 '''
 # Incluir um guia de como usar a função para o help() da função;
+
+def fatorial(n=int, show=False):
+    """
+    -> Calcula a fatorial.
+    :param n: Número a calcular
+    :param show: (Opcional) Indica se será mostrado ou não na tela o processo de cálculo do fatorial.
+    :return: Resultado da Fatorial
+    """
+    fat = 1
+
+    for c in range(n, 0, -1):
+        if show == True:
+            print(f'{c} ', end='')
+            if c > 1:
+                print(f'x ', end='')
+            else:
+                print(f'= ', end='')
+        fat *= c
+
+    return fat
+
+
+# Programa Principal
+print('-'*30)
+print(f'{"Fatorial":^30}')
+print('-'*30)
+
+print(fatorial(int(input('Número a ser calculado: ')), show=True))
+
+
+r = input('\nDeseja ver como funciona o programa? (s/n): ')
+
+if r != 'n':
+    help(fatorial)
+
+print('\nObrigado por usar o programa! :)')
