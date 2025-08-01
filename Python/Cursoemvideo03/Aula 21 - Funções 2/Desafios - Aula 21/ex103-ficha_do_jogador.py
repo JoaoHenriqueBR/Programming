69a701b56011a273ba400ecbc7f90dbc8a764ef2
João Henrique <70080335+JoaoHenriqueBR@users.noreply.github.com>
2025-08-01 19:31:25 -0300

    EX 103 (Finalizado)

--- Python/Cursoemvideo03/Aula 21 - Funções 2/Desafios - Aula 21/ex103-ficha_do_jogador.py
+++ Python/Cursoemvideo03/Aula 21 - Funções 2/Desafios - Aula 21/ex103-ficha_do_jogador.py
@@ -7,3 +7,20 @@ O programa deverá ser capaz de mostrar a ficha do jogador,
 mesmo que algum dado não tenha sido informado corretamente.
 '''
 
+def ficha(nome='<desconhecido>', gols=0):
+    print(f'O jogador {nome} fez {gols} gol(s) no campeonato.')
+
+
+# Programa principal
+n = str(input('Nome do jogador: '))
+g = str(input('Número de gols: '))
+
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+
+if n.strip() == '':
+    ficha(gols=g)
+else:
+    ficha(n, g)
