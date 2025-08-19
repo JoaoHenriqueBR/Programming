10d5e7fa5c097d8d098305acd53a6b3b4a3f55fa
JoaoHenriqueBR <jjb9362@gmail.com>
2025-08-19 11:04:27 -0300

    EX 105 (Finalizado)

--- Python/Cursoemvideo03/Aula 21 - Funções 2/Desafios - Aula 21/ex105-gerar_dict_notas.py
+++ Python/Cursoemvideo03/Aula 21 - Funções 2/Desafios - Aula 21/ex105-gerar_dict_notas.py
@@ -10,3 +10,34 @@ vai retornar um dicionário com as seguintes informações:
 
 Adicione também as docstrings da função.
 '''
+
+def notas(*n, sit=False):
+    """
+    -> Função para analisar várias notas.
+    :param n: Recebe uma ou mais notas.
+    :param sit: (OPCIONAL) Indica se deve ou não calcular a situação do aluno (Aprovado/Reprovado).
+    :return: dicionário com várias informações com base nas notas.
+    """
+
+
+    boletim = {'Quantidade':len(n),
+               'Maior':max(n),
+               'Menor':min(n),
+               'Média':sum(n)/len(n),
+               }
+    
+    if sit == True:
+        if boletim['Média'] >= 6:
+            boletim['Situação'] = 'Aprovado'
+        else:
+            boletim['Situação'] = 'Reprovado'
+    
+    return boletim
+
+
+# Programa Principal
+resp = notas(5.5, 2.5, 10, 6.5, sit=True)
+print(resp)
+
+# docstrings
+help(notas)
