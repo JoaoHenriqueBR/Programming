47829cea03ab38ff894b3503c4af4467a4261fb3
JoaoHenriqueBR <jjb9362@gmail.com>
2025-10-08 02:24:35 -0300

    DESAFIOS: Aula 22 - Módulos e Pacotes

---
+++ Python/Cursoemvideo03/Aula 22 - Módulos e Pacotes/Desafios - Aula 22/p1 - módulos/ex107/main.py
@@ -0,0 +1,11 @@
+'''
+Crie um módulo chamado moeda.py que tenha as funções incorporadas
+
+aumentar()
+diminuir()
+dobro()
+metade()
+
+Faça também um programa que importe esse módulo e use algumas dessas funções.
+'''
+
---
+++ Python/Cursoemvideo03/Aula 22 - Módulos e Pacotes/Desafios - Aula 22/p1 - módulos/ex110/main.py
@@ -0,0 +1,3 @@
+# Adicione ao módulo moeda.py criado nos módulos anterios,
+# uma função chamada resumo(), que mostra na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+
