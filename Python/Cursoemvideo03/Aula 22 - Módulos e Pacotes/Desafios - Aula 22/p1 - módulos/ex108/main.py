61c5f445c2c9a038dc08c4c08792ecdb3ddfa153
João Henrique <70080335+JoaoHenriqueBR@users.noreply.github.com>
2025-10-23 18:21:22 -0300

    EX 108 (Finalizado)

--- Python/Cursoemvideo03/Aula 22 - Módulos e Pacotes/Desafios - Aula 22/p1 - módulos/ex108/main.py
+++ Python/Cursoemvideo03/Aula 22 - Módulos e Pacotes/Desafios - Aula 22/p1 - módulos/ex108/main.py
@@ -1,3 +1,13 @@
 # Adapte o código do desafio 107, 
 # criando uma função adicional chamada moeda() que consiga mostrar os valores como um valor monetário formatado.
 
+from moeda import moeda, metade, dobro, aumentar, diminuir
+
+
+num = float(input('Digite um preço: R$ '))
+tax = int(input('Taxa de Aumento/Redução: '))
+
+print(f'A metade de {moeda(num)} é {moeda(metade(num))}')
+print(f'O dobro de {moeda(num)} é {moeda(dobro(num))}')
+print(f'Aumentando {tax}%, temos {moeda(aumentar(num, tax))}')
+print(f'Diminuindo {tax}%, temos {moeda(diminuir(num, tax))}')
