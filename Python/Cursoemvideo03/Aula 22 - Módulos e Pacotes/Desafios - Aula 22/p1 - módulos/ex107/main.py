6d25bc92d8fb9b423f6f856e4b50c2f1545ca4b1
João Henrique <70080335+JoaoHenriqueBR@users.noreply.github.com>
2025-10-23 18:21:08 -0300

    EX 107 (Finalizado)

--- Python/Cursoemvideo03/Aula 22 - Módulos e Pacotes/Desafios - Aula 22/p1 - módulos/ex107/main.py
+++ Python/Cursoemvideo03/Aula 22 - Módulos e Pacotes/Desafios - Aula 22/p1 - módulos/ex107/main.py
@@ -8,4 +8,12 @@ metade()
 
 Faça também um programa que importe esse módulo e use algumas dessas funções.
 '''
+import moeda
 
+num = int(input('Digite um preço: R$ '))
+tax = int(input('Taxa de Aumento/Redução: '))
+
+print(f'A metade de R$ {num:.2f} é R$ {moeda.metade(num):.2f}')
+print(f'O dobro de R$ {num:.2f} é R$ {moeda.dobro(num):.2f}')
+print(f'Aumentando {tax}%, temos R$ {moeda.aumentar(num, tax):.2f}')
+print(f'Diminuindo {tax}%, temos R$ {moeda.diminuir(num, tax):.2f}')
