94325ef3d68fd01aa03a219b190bd1a816b5553f
João Henrique <70080335+JoaoHenriqueBR@users.noreply.github.com>
2025-10-28 20:50:35 -0300

    EX 111 (Finalizado)

--- Python/Cursoemvideo03/Aula 22 - Módulos e Pacotes/Desafios - Aula 22/p2 - pacotes/ex111/main.py
+++ Python/Cursoemvideo03/Aula 22 - Módulos e Pacotes/Desafios - Aula 22/p2 - pacotes/ex111/main.py
@@ -3,3 +3,8 @@ Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamado mo
 
 Transfira todas as funções utilizadas nos desafios 107,108 e 109 para o primeiro pacote e mantenha tudo funcionando.
 '''
+
+from utilidadesCeV import moeda
+
+p = float(input('Digite o preço: R$ '))
+moeda.resumo(p, 20, 12)
