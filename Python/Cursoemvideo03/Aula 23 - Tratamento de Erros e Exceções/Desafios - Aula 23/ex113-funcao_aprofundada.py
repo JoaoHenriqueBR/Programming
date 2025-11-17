19ec68ae6f61bf85f04c3eee864cc6682f7f3362
João Henrique <70080335+JoaoHenriqueBR@users.noreply.github.com>
2025-11-17 17:52:08 -0300

    EX 113 (Finalizado)

--- Python/Cursoemvideo03/Aula 23 - Tratamento de Erros e Exceções/Desafios - Aula 23/ex113-funcao_aprofundada.py
+++ Python/Cursoemvideo03/Aula 23 - Tratamento de Erros e Exceções/Desafios - Aula 23/ex113-funcao_aprofundada.py
@@ -4,3 +4,31 @@ incluindo agora a possibilidade da digitação de um número de tipo inválido.
 Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
 """
 
+
+def leiaint(msg):
+    while True:
+        try:
+            n = int(input(msg))
+            return n
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        except KeyboardInterrupt:
+            print('\033[0;33mOperação cancelada! Valor anulado.\033[m')
+            return 0
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+            return n
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO! Digite um número real válido.\033[m')
+        except KeyboardInterrupt:
+            print('\033[0;33mOperação cancelada! Valor anulado.\033[m')
+            return 0
+
+
+inteiro = leiaint('Digite um número inteiro: ')
+real = leiaFloat('Digite um número real: ')
+print(f'Você digitou {inteiro} e {real}.')
