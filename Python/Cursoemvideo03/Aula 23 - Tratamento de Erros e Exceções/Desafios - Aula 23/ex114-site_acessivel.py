4c46e40f7e18d8ad51f58da30293d2136a24db1d
JoaoHenriqueBR <jjb9362@gmail.com>
2025-11-18 04:21:08 -0300

    EX 114 (Finalizado)

--- Python/Cursoemvideo03/Aula 23 - Tratamento de Erros e Exceções/Desafios - Aula 23/ex114-site_acessivel.py
+++ Python/Cursoemvideo03/Aula 23 - Tratamento de Erros e Exceções/Desafios - Aula 23/ex114-site_acessivel.py
@@ -1,2 +1,17 @@
 # Crie um código em Python que testa se o site Pudim está acessível pelo computador usado.
 
+import urllib
+import urllib.request
+
+
+def acessivel():
+    try:
+        site = urllib.request.urlopen('https://www.pudim.com.br/')
+    except urllib.error.URLError:
+        print('Não foi possivel acessar o site do pudim.')
+    else:
+        print('Site acessado com sucesso!')
+        print('\nBônus - Código do site: ')
+        print(site.read())
+
+acessivel()
