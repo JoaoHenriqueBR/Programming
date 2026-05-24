3b2497e89ffe967e051fb5937ad51d7460b51a28
JoaoHenriqueBR <jjb9362@gmail.com>
2026-05-24 02:29:52 -0300

    Java10x: Classes e objetos - Tudo é um objeto no Java!

---
+++ Java/Fiasco/Java10x/src/NivelIntermediario/Main.java
@@ -0,0 +1,26 @@
+package Java10x.src.NivelIntermediario;
+
+public class Main {
+    public static void main(String[] args) {
+//        Criar o ninja naruto (Objeto)
+        Ninja Naruto = new Ninja();
+
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.idade = 17;
+        Naruto.aldeia = "Aldeia da Folha";
+
+//        Criar ninja Sasuke (Objeto)
+        Ninja Sasuke = new Ninja();
+
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.idade = 18;
+        Sasuke.aldeia = "Aldeia da Folha";
+
+//        Criar a Sakura
+        Ninja Sakura = new Ninja();
+
+        Sakura.nome = "Sakura Haruno";
+        Sakura.idade = 18;
+        Sakura.aldeia = "Aldeia da Folha";
+    }
+}
---
+++ Java/Fiasco/Java10x/src/NivelIntermediario/Ninja.java
@@ -0,0 +1,7 @@
+package Java10x.src.NivelIntermediario;
+
+public class Ninja {
+    String nome;
+    String aldeia;
+    int idade;
+}
